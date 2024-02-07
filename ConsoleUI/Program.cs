c39c890613ff2ecbc582b7c560c5257ad3589847
Pierre Plourde <pierre@spartancsharp.net>
2024-02-07 14:45:25 -0500

    Create Code
    Build a Console Guest Book. Ask the user for their name and how many are in their party. Keep track of how many people are at the party. At the end, print out the guest list and the total number of guests.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,6 +10,72 @@ namespace ConsoleUI
 	{
 		static void Main(string[] args)
 		{
+			//Start counting guests at 0
+			int totalguests = 0;
+			// Initialize guest list
+			List<string> guestlist = new List<string>();
+			// Initialize done checker
+			string done;
+
+			do
+			{
+				//Get user name
+				string guestname = GetGuestName();
+				//Add name to guest list
+				guestlist.Add(guestname);
+				//Get # of people in party
+				int partysize = GetPartySize();
+				//Add # of people to total guests
+				totalguests += partysize;
+				//Ask if done
+				Console.Write("Done (yes/no): ");
+				done = Console.ReadLine();
+				//If not done loop back to get user name
+				//If done move on
+			} while ( done.ToLower() != "yes" );
+
+			//Print out list of people
+			//Print out total # of people
+			DisplayGuestList(guestlist, totalguests);
+
+			_ = Console.ReadLine();
+		}
+
+		private static string GetGuestName()
+		{
+			Console.Write("What is the name of your party: ");
+			string output = Console.ReadLine();
+			return output;
+		}
+
+		private static int GetPartySize()
+		{
+			bool issizevalid;
+			int output;
+
+			do
+			{
+				Console.Write("How many people in your party: ");
+				string partysize = Console.ReadLine();
+				issizevalid = int.TryParse(partysize, out output);
+				if ( !issizevalid )
+				{
+					Console.WriteLine("Please enter the number using digits.");
+				}
+			} while ( !issizevalid );
+
+			return output;
+		}
+
+		private static void DisplayGuestList(List<string> guestList, int totalGuests)
+		{
+			Console.WriteLine("Party Guest List");
+			foreach ( string guest in guestList )
+			{
+				Console.WriteLine(guest);
+			}
+
+			Console.WriteLine($"Total Guests: {totalGuests}");
 		}
 	}
 }
